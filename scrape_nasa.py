23002ca4009631fe656a95ddcefef27c922ad724
ShadeP <pettewayshade@gmail.com>
2019-02-18 21:38:24 -0600

    Added if statements to scrape script

--- scrape_nasa.py
+++ scrape_nasa.py
@@ -15,8 +15,10 @@ def scrape_info():
     line_in_list = read_list.split("\n")
 
     browser = init_browser()
-    
+
+    counter = 0
     for url in line_in_list:
+            counter +=1
             browser.visit(url)
 
             time.sleep(1)
@@ -27,19 +29,35 @@ def scrape_info():
 
 
             #get data we are looking for
-            news_title = ""
+            if counter == 1:
+                #get data from first site
+                news_title = ""
+                news_p = ""
 
-            news_p = ""
+            if counter == 2:
+                #get data from second site
+                featured_image_url = ""
+
+            if counter == 3:
+                #get data from third site
+                mars_weather = ""
+
+            if counter == 4:
+                #get data from 4th site:
+                images = []
 
 
-            #dict to store data
-            mars_data = {
-                    "news_title": news_tile,
-                    "news_p": news_p
-            }
-
-            #close browser session after scraping
+            #close brower session
             browser.quit()
+
+    #dict to store data
+    mars_data = {
+            "news_title": news_tile,
+            "news_p": news_p,
+            "featured_image_url": featured_image_url,
+            "images": images
+    }
+
 
     #return the results
     return mars_data
